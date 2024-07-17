c6598e71473afa9d156ae32ccaaeb42b7228b70d
RobertinoLirusso <robertinolirusso3@gmail.com>
2024-07-17 16:28:50 -0300

    new changes

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://football-club-api-ybdq.onrender.com/club'
+    apiUrl: 'https://football-club-api-ybdq.onrender.com/club/'
   };
